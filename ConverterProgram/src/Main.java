4a196f49ff438f2906df48651a62e7aa0cd76900
ben-platt23 <86609189+ben-platt23@users.noreply.github.com>
2021-11-15 19:54:31 -0500

    added test comment to learn pushing

--- ConverterProgram/src/Main.java
+++ ConverterProgram/src/Main.java
@@ -2,6 +2,7 @@
 @author Ben Platt
 @date version 1: 11/5/2020
 updated with binary and decimal conversion: 10/25/2021
+pushed to GitHub 11/15/2021
 */
 class Main {
   public static void main(String[] args) {
